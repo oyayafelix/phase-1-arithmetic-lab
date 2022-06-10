d50292ad963f34dff989c547e192653328873236
oyayafelix <oyayafelix@gmail.com>
2022-06-10 16:03:05 +0300

    solution

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+//random interger greater than zero
+const random = Math.floor(Math.random() * 100) + 1; 
+
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+//return highest number
+const max = Math.max(5,20,10,4);
